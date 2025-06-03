2d27f90bd37c25831ce3bf60b7222b9023b4f2bd
Kirsanov Alex <116355979+AlexMaster001@users.noreply.github.com>
2025-06-03 20:31:31 +0300

    fix comma and import

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,13 +5,13 @@ import genDiff from '../src/index.js';
 import parse from '../src/parsers.js';
 import makeFormat from '../src/formatters/index.js';
 import plain from '../src/formatters/plain.js';
-import stylish from '../src/formatters/stylish';
+import stylish from '../src/formatters/stylish.js';
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const getFixturePath = filename => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = filename => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 const testFile = readFile('result.txt');
 const fileJson1 = './__fixtures__/file1.json';
